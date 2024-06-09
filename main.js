cf7dc44680c37a61324db3b9f52cca7f33f0d6f7
MartinMedulan <xmedulan@mendelu.cz>
2024-06-09 16:51:00 +0200

    finalni upravy - oprava restartu, pridani reactionTime timeru

--- main.js
+++ main.js
@@ -1,5 +1,38 @@
 let lives = 10;  // Počet životů hráče
 let initialPositions = {};  // Objekt pro uložení počátečních pozic míčků
+let reactionTimes = [];  // Pole pro uložení reakcí hráče
+let startTime;  // Proměnná pro uložení počátečního času reakce
+let globalAverageReactionTime = 999999;  // Proměnná pro globální průměrnou reakční dobu (počáteční hodnota je vysoká)
+
+function startReactionTimer() {
+    startTime = Date.now(); // Uloží aktuální čas (v milisekundách) do proměnné startTime pro pozdější výpočet reakční doby.
+}
+
+function stopReactionTimer() {
+    if (!startTime) return; // Pokud startTime není nastaven (tj. reakční timer nebyl spuštěn), funkce se ukončí.
+
+    let reactionTime = Date.now() - startTime; // Vypočítá reakční dobu jako rozdíl mezi aktuálním časem a startTime.
+
+    reactionTimes.push(reactionTime); // Přidá vypočítanou reakční dobu do pole reactionTimes.
+
+    let lastReactionTimeDisplay = document.querySelector('#lastReactionTimeDisplay'); // Vyhledá prvek pro zobrazení reakční doby.
+    lastReactionTimeDisplay.setAttribute('text', 'value', 'Reaction Time: ' + reactionTime.toFixed(2) + 'ms'); // Aktualizuje text v prvku s poslední reakční dobou.
+
+    startTime = null; // Resetuje startTime na null, aby indikoval, že timer není spuštěn.
+
+    console.log("Reaction Time: " + reactionTime + "ms"); // Vypíše reakční dobu do konzole.
+    calculateAverageReactionTime(); // Zavolá funkci pro výpočet a zobrazení průměrné reakční doby.
+}
+
+function calculateAverageReactionTime() {
+    let sum = reactionTimes.reduce((a, b) => a + b, 0); // Sečte všechny hodnoty v poli reactionTimes.
+    let avg = sum / reactionTimes.length; // Vypočítá průměrnou reakční dobu jako součet všech hodnot dělený počtem hodnot v poli.
+    globalAverageReactionTime = avg;  // Uloží průměrnou reakční dobu do globální proměnné
+    let reactionTimeDisplay = document.querySelector('#reactionTimeDisplay'); // Vyhledá prvek pro zobrazení průměrné reakční doby.
+    reactionTimeDisplay.setAttribute('text', 'value', 'Average Reaction Time: ' + avg.toFixed(2) + 'ms'); // Aktualizuje text v prvku s průměrnou reakční dobou.
+
+    console.log("Average Reaction Time: " + avg.toFixed(2) + "ms"); // Vypíše průměrnou reakční dobu (zaokrouhlenou na dvě desetinná místa) do konzole.
+}
 
 // Funkce pro přidání bodu při zásahu míčku
 function addScore(ball) {
@@ -14,7 +47,7 @@ function addScore(ball) {
         var score = parseInt(scoreboard.getAttribute('text').value.split(': ')[1]) + 1;  // Zvýší skóre o 1
         scoreboard.setAttribute('text', 'value', 'Score: ' + score);  // Aktualizuje text se skóre
 
-        //ball.emit('hit');  // Spustí událost "hit" na míčku (pro explozi)
+        stopReactionTimer();  // Zastaví měření reakční doby
     } else if (ball.getAttribute('material').color === 'green') {  // Pokud je barva míčku zelená
         var badHitSoundEntity = document.querySelector('#incorrectHitSound');  // Vyhledá element se zvukem špatného zásahu
         badHitSoundEntity.components.sound.playSound();  // Přehrát zvuk špatného zásahu
@@ -55,6 +88,8 @@ function flashRandomBall() {
         distance: 5,
         decay: 2
     });
+
+    startReactionTimer();  // Spustí měření reakční doby
 
     // Po krátké chvíli (700 ms) vrátíme barvu zpět na zelenou
     setTimeout(function () {
@@ -104,14 +139,21 @@ function gameOver() {
     document.querySelector('#balls').setAttribute('visible', 'false');  // Skryje všechny míčky
     document.querySelector('#hearts').setAttribute('visible', 'false');  // Skryje všechny srdce
     document.querySelector('#scoreboard').setAttribute('visible', 'false');  // Skryje ukazatel skóre
-    document.querySelector('#leftHand').setAttribute('visible', 'false');  // Skryje levou ruku
-    document.querySelector('#rightHand').setAttribute('visible', 'false');  // Skryje pravou ruku
 
     var score = document.querySelector('#scoreboard').getAttribute('text').value.split(': ')[1];  // Získá aktuální skóre
     document.querySelector('#gameOver').setAttribute('visible', 'true');  // Zobrazí text "Game Over"
     document.querySelector('#finalScore').setAttribute('text', 'value', 'YOUR SCORE: ' + score);  // Zobrazí konečné skóre
     document.querySelector('#finalScore').setAttribute('visible', 'true');  // Zviditelní konečné skóre
+
     document.querySelector('#restartButton').setAttribute('visible', 'true');  // Zviditelní tlačítko restartu
+
+    document.querySelector('#reactionTimeDisplay').setAttribute('visible', 'false');  // Zneviditelní průměrný čas reakce
+    document.querySelector('#lastReactionTimeDisplay').setAttribute('visible', 'false');  // Zneviditelní poslední reakční čas
+
+    // Aktualizuje a zviditelní prvek pro zobrazení průměrné reakční doby
+    let finalAverageReactionTimeDisplay = document.querySelector('#finalAverageReactionTime');
+    finalAverageReactionTimeDisplay.setAttribute('text', 'value', 'AVERAGE REACTION TIME: ' + globalAverageReactionTime.toFixed(2) + 'ms');
+    finalAverageReactionTimeDisplay.setAttribute('visible', 'true');
 }
 
 // Funkce pro restart hry
@@ -132,6 +174,15 @@ function restartGame() {
     document.querySelectorAll('#hearts a-image').forEach(function(heart) {
         heart.setAttribute('visible', 'true');  // Zviditelní všechna srdce (ukazatele životů)
     });
+    document.querySelector('#finalAverageReactionTime').setAttribute('visible', 'false');  // Skryje průměrný reakční čas
+    document.querySelector('#reactionTimeDisplay').setAttribute('visible', 'true');  // Zneviditelní průměrný čas reakce
+    document.querySelector('#lastReactionTimeDisplay').setAttribute('visible', 'true');  // Zneviditelní poslední reakční čas
+    reactionTimes = [];
+    globalAverageReactionTime = 0;
+
+    document.querySelector('#reactionTimeDisplay').setAttribute('text', 'value', 'Average Reaction Time: 0ms');  // Zneviditelní průměrný čas reakce
+    document.querySelector('#lastReactionTimeDisplay').setAttribute('text', 'value', 'Reaction Time: 0ms');  // Zneviditelní poslední reakční čas
+
 }
 
 // Spuštění blikání míčků v náhodných intervalech 1-3 sekundy
@@ -148,12 +199,23 @@ function getRandomNumber(min, max) {
     return Math.random() * (max - min) + min;  // Vrátí náhodné číslo v daném rozsahu
 }
 
+// Funkce pro uložení počátečních pozic míčků
+document.querySelectorAll('.ball').forEach(function(ball, index) {
+    initialPositions[index] = ball.getAttribute('position');  // Uloží počáteční pozici každého míčku
+});
+
 // Funkce pro nastavení nové náhodné pozice s omezením
 function setNewRandomPosition(ball, index) {
-    let balls = document.querySelectorAll('.ball');  // Vyhledá všechny míčky
     let initialPosition = initialPositions[index];  // Získá počáteční pozici míčku
     let newPosition;
     let isColliding;
+    const maxDistance = 0.01; // Maximální vzdálenost, kterou se míček může posunout od své počáteční pozice
+    const boundaries = {
+        xMin: initialPosition.x - maxDistance,
+        xMax: initialPosition.x + maxDistance,
+        yMin: initialPosition.y,  //nahoru se nesmí posouvat
+        yMax: initialPosition.y,  //nahoru se nesmí posouvat
+    };
 
     do {
         newPosition = {
@@ -162,9 +224,15 @@ function setNewRandomPosition(ball, index) {
             z: initialPosition.z  // Zanechá původní pozici v ose Z
         };
 
+        // Zkontroluje, zda nová pozice nepřekračuje hranice
+        if (newPosition.x < boundaries.xMin) newPosition.x = boundaries.xMin;
+        if (newPosition.x > boundaries.xMax) newPosition.x = boundaries.xMax;
+        if (newPosition.y < boundaries.yMin) newPosition.y = boundaries.yMin;
+        if (newPosition.y > boundaries.yMax) newPosition.y = boundaries.yMax;
+
         isColliding = false;
 
-        balls.forEach(otherBall => {
+        document.querySelectorAll('.ball').forEach(otherBall => {
             if (otherBall !== ball) {  // Pokud to není stejný míček
                 let otherPosition = otherBall.getAttribute('position');  // Získá pozici jiného míčku
                 let distance = Math.sqrt(
@@ -182,7 +250,7 @@ function setNewRandomPosition(ball, index) {
     ball.setAttribute('animation__position', {
         property: 'position',
         to: `${newPosition.x} ${newPosition.y} ${newPosition.z}`,  // Nastaví novou pozici
-        dur: getRandomNumber(3000, 5000),  // Trvání animace mezi 3 a 5 sekundami
+        dur: getRandomNumber(2000, 4000),  // Trvání animace mezi 2 a 4 sekundami
         easing: 'easeInOutSine',  // Typ easing animace
         loop: false  // Animace se neopakuje
     });
@@ -192,7 +260,7 @@ function setNewRandomPosition(ball, index) {
 document.querySelectorAll('.ball').forEach(function(ball, index) {
     setInterval(function() {
         setNewRandomPosition(ball, index);  // Nastaví novou náhodnou pozici pro každý míček
-    }, getRandomNumber(1000, 2000));  // Interval mezi 1 a 2 sekundami
+    }, getRandomNumber(2000, 4000));  // Interval mezi 2 a 4 sekundami
 });
 
 // Komponenta pro omezení pohybu
@@ -213,57 +281,54 @@ AFRAME.registerComponent('movement-limiter', {
     }
 });
 
-
-
-
+// Komponenta pro ovládání pohybu pomocí palcových joysticků na ovladačích Oculus Touch
 AFRAME.registerComponent('oculus-thumbstick-controls', {
     schema: {
-        acceleration: { default: 45 },
-        rigSelector: { default: "#cameraRig" },
-        fly: { default: false },
-        controllerOriented: { default: false },
-        adAxis: { default: 'x' },
-        wsAxis: { default: 'z' },
-        enabled: { default: true },
-        adEnabled: { default: true },
-        adInverted: { default: false },
-        wsEnabled: { default: true },
-        wsInverted: { default: false }
+        acceleration: { default: 45 },  // Rychlost zrychlení
+        rigSelector: { default: "#cameraRig" },  // Selektor pro hlavní entitu kamery
+        fly: { default: false },  // Povoluje létání (nejen pohyb po zemi)
+        controllerOriented: { default: false },  // Použití orientace ovladače pro pohyb
+        adAxis: { default: 'x' },  // Osa pro pohyb vlevo/vpravo
+        wsAxis: { default: 'z' },  // Osa pro pohyb vpřed/vzad
+        enabled: { default: true },  // Povolit/zakázat komponentu
+        adEnabled: { default: true },  // Povolit/zakázat pohyb vlevo/vpravo
+        adInverted: { default: false },  // Inverze osy vlevo/vpravo
+        wsEnabled: { default: true },  // Povolit/zakázat pohyb vpřed/vzad
+        wsInverted: { default: false }  // Inverze osy vpřed/vzad
     },
     init: function () {
-        this.easing = 1.1;
-        this.velocity = new THREE.Vector3(0, 0, 0);
-        this.tsData = new THREE.Vector2(0, 0);
-        this.el.addEventListener('axismove', this.onAxisMove.bind(this));
+        this.easing = 1.1;  // Koeficient pro plynulé zpomalení
+        this.velocity = new THREE.Vector3(0, 0, 0);  // Vektor pro uložení aktuální rychlosti
+        this.tsData = new THREE.Vector2(0, 0);  // Vektor pro uložení dat z joysticku
+        this.el.addEventListener('axismove', this.onAxisMove.bind(this));  // Přidání event listeneru pro pohyb joysticku
     },
     onAxisMove: function (event) {
         const axis = event.detail.axis;
         const cameraRig = document.querySelector(this.data.rigSelector);
 
         if (event.target.components['oculus-touch-controls'].data.hand === 'left') {
-            cameraRig.object3D.position.x += axis[2] * 0.1; // Horizontal movement
-            cameraRig.object3D.position.z += axis[3] * 0.1; // Forward/Backward movement
+            cameraRig.object3D.position.x += axis[2] * 0.01; // Pohyb vlevo/vpravo
+            cameraRig.object3D.position.z += axis[3] * 0.01; // Pohyb vpřed/vzad
         }
 
         if (event.target.components['oculus-touch-controls'].data.hand === 'right') {
-            cameraRig.object3D.rotation.y -= axis[2] * 0.05; // Yaw rotation
+            cameraRig.object3D.rotation.y -= axis[2] * 0.01; // Otáčení
         }
     }
 });
 
 document.addEventListener('DOMContentLoaded', () => {
     document.querySelectorAll('[oculus-touch-controls]').forEach(el => {
-        el.setAttribute('oculus-thumbstick-controls', '');
+        el.setAttribute('oculus-thumbstick-controls', '');  // Přidání komponenty oculus-thumbstick-controls pro každý ovladač
     });
 });
 
-
-
-
+// Přidání události pro tlačítko A na levém ovladači pro restart hry
 document.querySelector('[oculus-touch-controls="hand: left"]').addEventListener('abuttondown', function(evt) {
     restartGame();
 });
+
+// Přidání události pro tlačítko A na pravém ovladači pro restart hry
 document.querySelector('[oculus-touch-controls="hand: right"]').addEventListener('abuttondown', function(evt) {
     restartGame();
 });
-
